cc2ddfda77a23e30cff6f7e50ead96ae0e3b92f8
nivoix <jnivoix@yahoo.fr>
2024-01-22 12:45:31 +0100

    footer ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,44 @@
+import "./Footer.scss";
+import { IoLogoFacebook } from "react-icons/io5";
+import { IoLogoGoogle } from "react-icons/io5";
+import { IoLogoTwitter } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer>
+      <div className="footerContact">
+        <div className="footerAdress">
+          <p>ACAN</p>
+          <p> 21 rue des Charrons 08090 AIGLEMONT</p>
+          <p> contact@acan-08.fr</p>
+        </div>
+        <div className="footerLink">
+          <NavLink className="btnEmail" href="mailto:contact@acan-08.fr">
+            ENVOYER UN MAIL
+          </NavLink>
+          <div className="cardIcons">
+            <NavLink className="iconLink" to="https://twitter.com">
+              <IoLogoTwitter />
+            </NavLink>
+            <NavLink className="iconLink" to="https://www.facebook.com/">
+              <IoLogoFacebook />
+            </NavLink>
+            <NavLink className="iconLink" to="https://news.google.com/">
+              <IoLogoGoogle />
+            </NavLink>
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
+        <span>
+          © 2024 SARL ACAN au capital de 7700 € - 21, rue des Charrons - 08090
+          AIGLEMONT
+        </span>
+        <span>{`SIRET 45165072500018 Activité (Code NAF ou APE) 8121Z`}</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,23 @@
 function Home() {
-  return <div>Page Home</div>;
+  return (
+    <>
+      Page Home
+      <div className="homeBanner">Banner</div>
+      <div>Carrousel</div>
+      <h3>{`NOS DOMAINES D'INTERVENTION`}</h3>
+      <div>Card</div>
+      <div>Card</div>
+      <div>Card</div>
+      <section>
+        Parce que la satisfaction de nos clients est au cœur de nos
+        préoccupations.
+      </section>
+      <aside>{`La Vision ACAN
+Créée en 2004, ACAN est une entreprise de propreté et de destruction ou régulation de nuisibles de dimension régionale qui compte ses clients répartis principalement à Charleville-Mézières et dans toutes les Ardennes.
+Nous sommes engagés depuis longtemps à obtenir une politique de qualité comme en témoignent nos nombreux clients dans les domaines respectifs de l'industrie, du commerce, des métiers de bouche et des collectivités locales.
+Choisir ACAN, c'est l'assurance d'un service de proximité que peut offrir un partenaire régional, avec les moyens matériels et humains dignes des plus grosses structures.`}</aside>
+    </>
+  );
 }
 
 export default Home;
